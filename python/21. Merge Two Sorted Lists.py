bbd5cf51da88e4d7a9c7e7937f362dcd6c4776b6
Ray08charles <45870004+red574890@users.noreply.github.com>
2023-05-13 10:31:28 -0400

    Update 21. Merge Two Sorted Lists.py

--- python/21. Merge Two Sorted Lists.py
+++ python/21. Merge Two Sorted Lists.py
@@ -5,38 +5,31 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        new=[]
+       head = ListNode(0)
+       curr = head
+       while (list1 != None or list2 != None):
+           
+           if(list1 == None):
+               curr.next = list2
+               curr = curr.next
+               list2 = list2.next
+               continue
+           elif(list2 == None):
+               curr.next = list1
+               curr = curr.next
+               list1 = list1.next
+               continue
+               
+           else:
+               if(list1.val <= list2.val):
+                   curr.next = list1
+                   curr = curr.next
+                   list1 = list1.next
+               else:
+                    curr.next = list2
+                    curr = curr.next
+                    list2 = list2.next
 
-        while(list1!=None or list2!=None):
-            
-            if(list1==None and list2!=None):
-                new.append(list2.val)
-                list2=list2.next
-            
-            elif(list1!=None and list2==None):
-                new.append(list1.val)
-                list1=list1.next
-            
-            else:
-                if(list1.val>list2.val):
-                    new.append(list2.val)
-                    list2=list2.next
-                else:
-                    new.append(list1.val)
-                    list1=list1.next
-
-        for i in range(len(new)):
-            if(i==0):
-                first=ListNode(new[len(new)-i-1],None)
-            else:
-                first=ListNode(new[len(new)-i-1],first)
-        
-        if(len(new)==0):
-            return None
-    
-        return first
-            
-            
-            
-        
+       head = head.next
+       return head
+         
