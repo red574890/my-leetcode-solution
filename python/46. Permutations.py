eaf851a36e8e6bd778b5efd9b4cdf3687bc9341b
Ray08charles <45870004+red574890@users.noreply.github.com>
2024-10-19 11:37:12 -0400

    Update 46. Permutations.py

--- python/46. Permutations.py
+++ python/46. Permutations.py
@@ -28,3 +28,27 @@ class Solution:
 
         return res
                 
+
+
+##------------#   20241019 Ray H new code
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def DFS(path,remain):
+
+            if(len(remain) == 0):
+                res.append(path)
+                return 
+            else:
+                for i in range(len(remain)):
+                    tmp = remain.copy()
+                    path_tmp = path.copy()
+                    current = tmp.pop(i)
+                    path_tmp.append(current)
+                    DFS(path_tmp,tmp)
+            
+ 
+        DFS([],nums)
+
+        return res
